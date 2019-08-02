e23dfc1c27465871b2e5767056cedee6811137a2
edwinsatya <edwinsatyayudistira@gmail.com>
2019-08-02 14:13:23 +0700

    exercises-2

---
+++ exercises-2.js
@@ -0,0 +1,11 @@
+//Mengakses Nilai Dalam Array
+
+function balikString (string) {
+    var temp = '';
+    for (var i = string.length-1; i >= 0; i--) {
+        temp += string[i];
+    }
+    return temp;
+};
+
+console.log(balikString('hello world!'));
