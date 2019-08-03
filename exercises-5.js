cafeeae185d03f847c78da949800d52b791a156b
edwinsatya <edwinsatyayudistira@gmail.com>
2019-08-03 13:43:50 +0700

    exercises-5

---
+++ exercises-5.js
@@ -0,0 +1,21 @@
+//Logic Challenge - Palindrome
+
+function palindrome(kata) {
+    // you can only write your code here!
+    var newKata = '';
+    for (var i = kata.length-1; i >= 0; i--) {
+        newKata += kata[i];
+    }
+    if (kata === newKata) {
+        return true;
+    } else {
+        return false;
+    }
+};
+  
+// TEST CASES
+console.log(palindrome('katak')); // true
+console.log(palindrome('blanket')); // false
+console.log(palindrome('civic')); // true
+console.log(palindrome('kasur rusak')); // true
+console.log(palindrome('mister')); // false
