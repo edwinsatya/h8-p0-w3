d479c5403b9d3b11870a957f1fa863e8d2c437a4
edwinsatya <edwinsatyayudistira@gmail.com>
2019-08-08 18:28:10 +0700

    exercises-14

---
+++ exercises-14.js
@@ -0,0 +1,28 @@
+//Logic Challenge - Mengelompokkan Angka
+
+function mengelompokkanAngka(arr) {
+    // you can only write your code here!
+    var genap = [];
+    var ganjil = [];
+    var kelipatan3 = [];
+    var result = [];
+
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] % 3 == 0) {
+            kelipatan3[kelipatan3.length] = arr[i];
+        } else if (arr[i] % 2 == 0) {
+            genap[genap.length] = arr[i];
+        } else if (arr[i] % 2 != 0) {
+            ganjil[ganjil.length] = arr[i];
+        } 
+        
+    }
+    result.push(genap, ganjil, kelipatan3);
+    return result;
+};
+  
+  // TEST CASES
+  console.log(mengelompokkanAngka([2, 4, 6])); // [ [2, 4], [], [6] ]
+  console.log(mengelompokkanAngka([1, 2, 3, 4, 5, 6, 7, 8, 9])); // [ [ 2, 4, 8 ], [ 1, 5, 7 ], [ 3, 6, 9 ] ]
+  console.log(mengelompokkanAngka([100, 151, 122, 99, 111])); // [ [ 100, 122 ], [ 151 ], [ 99, 111 ] ]
+  console.log(mengelompokkanAngka([])); // [ [], [], [] ]
